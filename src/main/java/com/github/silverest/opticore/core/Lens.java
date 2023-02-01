bf83424d275e592be441bc46674faf34b8b2bce9
silverest12 <silverest12@gmail.com>
2023-02-01 08:42:53 +0100

    forgot gitignore

--- src/main/java/com/github/silverest/opticore/core/Lens.java
+++ src/main/java/com/github/silverest/opticore/core/Lens.java
@@ -1,5 +1,6 @@
 package com.github.silverest.opticore.core;
 
+import java.nio.file.Files;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
@@ -13,37 +14,39 @@ public class Lens <A, B> {
         this.setter = setter;
     }
 
-    public static <A, B> Lens<A, B> of(Function<A, B> getter, Function<B, Function<A, A>> setter) {
+    public static <A, B> Lens<A, B> of(final Function<A, B> getter, final Function<B, Function<A, A>> setter) {
         return new Lens<>(getter, setter);
     }
 
-    public static <A, B> Lens<A, B> of(Function<A, B> getter, BiFunction<B, A, A> setter) {
+    public static <A, B> Lens<A, B> of(final Function<A, B> getter, final BiFunction<B, A, A> setter) {
         return new Lens<>(getter, b -> a -> setter.apply(b, a));
     }
 
-    public static <A, B> Lens<A, B> of(Function<A, B> getter, BiConsumer<B, A> setter) {
-        return new Lens<>(getter, b -> a -> {
-            setter.accept(b, a);
-            return a;
-        });
-    }
-
-    public B get(A a) {
+    public B get(final A a) {
         return getter.apply(a);
     }
 
-    public A set(B b, A a) {
+    public A set(final B b, final A a) {
         return setter.apply(b).apply(a);
     }
 
-    public A modify(Function<B, B> f, A a) {
+    public A modify(final Function<B, B> f, A a) {
         return set(f.apply(get(a)), a);
     }
 
-    public <C> Lens<A, C> compose(Lens<B, C> other) {
+    // andThen :: Lens a b -> Lens b c -> Lens a c
+    public <C> Lens<A, C> andThen(final Lens<B, C> other) {
         return Lens.of(
-                a -> other.getter.apply(getter.apply(a)),
-                c -> a -> setter.apply(other.setter.apply(c).apply(getter.apply(a))).apply(a)
+                a -> other.get(get(a)),
+                c -> a -> modify(b -> other.set(c, b), a)
+        );
+    }
+
+    // compose :: Lens a b -> Lens c a -> Lens c b
+    public <C> Lens<C, B> compose(final Lens<C, A> other) {
+        return Lens.of(
+                c -> get(other.get(c)),
+                b -> c -> other.modify(a -> set(b, a), c)
         );
     }
 }
--- src/test/java/com/github/silverest/opticore/LensTest.java
+++ src/test/java/com/github/silverest/opticore/LensTest.java
@@ -7,27 +7,63 @@ import static org.junit.Assert.assertEquals;
 
 public class LensTest {
 
-    private record Person(String name, int age) {
-    }
+  private record Person(String name, int age, Address address) {}
 
-    @Test
-    public void setThenGet() {
-        Lens<Person, String> nameLens = Lens.of(Person::name, name -> person -> new Person(name, person.age()));
-        Person person = new Person("John", 30);
-        assertEquals("Jane", nameLens.get(nameLens.set("Jane", person)));
-    }
+  private record Address(String street, int number) {}
 
-    @Test
-    public void getThenSet() {
-        Lens<Person, String> nameLens = Lens.of(Person::name, name -> person -> new Person(name, person.age()));
-        Person person = new Person("John", 30);
-        assertEquals(person, nameLens.set(nameLens.get(person), person));
-    }
+  @Test
+  public void setThenGet() {
+    Lens<Person, String> nameLens =
+        Lens.of(Person::name, name -> person -> new Person(name, person.age(), person.address()));
+    Person person = new Person("John", 30, new Address("Main Street", 1));
+    assertEquals("Jane", nameLens.get(nameLens.set("Jane", person)));
+  }
 
-    @Test
-    public void setThenSet() {
-        Lens<Person, String> nameLens = Lens.of(Person::name, name -> person -> new Person(name, person.age()));
-        Person person = new Person("John", 30);
-        assertEquals("John2", nameLens.get(nameLens.set("John2", nameLens.set("Jane", person))));
-    }
+  @Test
+  public void getThenSet() {
+    Lens<Person, String> nameLens =
+        Lens.of(Person::name, name -> person -> new Person(name, person.age(), person.address()));
+    Person person = new Person("John", 30, new Address("Main Street", 1));
+    assertEquals(person, nameLens.set(nameLens.get(person), person));
+  }
+
+  @Test
+  public void setThenSet() {
+    Lens<Person, String> nameLens =
+        Lens.of(Person::name, name -> person -> new Person(name, person.age(), person.address()));
+    Person person = new Person("John", 30, new Address("Main Street", 1));
+    assertEquals("John2", nameLens.get(nameLens.set("John2", nameLens.set("Jane", person))));
+  }
+
+  @Test
+  public void andThen() {
+    Lens<Person, Address> addressLens =
+        Lens.of(
+            Person::address, address -> person -> new Person(person.name(), person.age(), address));
+    Lens<Address, String> streetLens =
+        Lens.of(Address::street, street -> address -> new Address(street, address.number()));
+
+    Person person = new Person("John", 30, new Address("Main Street", 1));
+    assertEquals(
+        "Main Street 2",
+        addressLens
+            .andThen(streetLens)
+            .get(addressLens.andThen(streetLens).set("Main Street 2", person)));
+  }
+
+  @Test
+  public void compose() {
+    Lens<Person, Address> addressLens =
+        Lens.of(
+            Person::address, address -> person -> new Person(person.name(), person.age(), address));
+    Lens<Address, String> streetLens =
+        Lens.of(Address::street, street -> address -> new Address(street, address.number()));
+
+    Person person = new Person("John", 30, new Address("Main Street", 1));
+    assertEquals(
+        "Main Street 2",
+        streetLens
+            .compose(addressLens)
+            .get(streetLens.compose(addressLens).set("Main Street 2", person)));
+  }
 }
